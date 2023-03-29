9cfd5c032349bf637eeca10dde51e19313b843ca
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2023-03-28 17:45:03 -0700

    re-upload of old project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://pk:test1234@cluster0.kosk9yx.mongodb.net/note-tuts?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
